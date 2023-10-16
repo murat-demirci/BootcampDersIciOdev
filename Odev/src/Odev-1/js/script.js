8691cd197244136566bb3866bc477e053363f041
murat-demrici <ddemircimuratt@gmail.com>
2023-10-16 15:58:13 +0300

    odev1

--- Odev/src/Odev-1/js/script.js
+++
@@ -1,5 +0,0 @@
-const sideBarClick = (event) =>{
-    const activeSideMenuElement = document.querySelector(".active");
-    activeSideMenuElement.classList.remove("active") 
-    event.target.classList.add("active")
-}
